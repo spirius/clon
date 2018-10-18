3cf993fa00dfdcd476e864e01e7391441665bc15
Vahe Sahakyan <vahe@spir.me>
2018-10-19 01:00:08 +0200

    Remove template based output

--- cmd/clon/cmd/output.go
+++ cmd/clon/cmd/output.go
@@ -3,10 +3,16 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"sort"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/spirius/clon/internal/pkg/cfn"
 	"github.com/spirius/clon/pkg/clon"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/fatih/color"
 	"github.com/juju/errors"
 )
 
@@ -31,6 +37,23 @@ type output interface {
 type outputCommon struct {
 	data interface{}
 	typ  int
+}
+
+func outputStatus(s string) string {
+	if s == cloudformation.StackStatusRollbackInProgress {
+		return color.HiRedString(s)
+	} else if s == cloudformation.StackStatusRollbackComplete {
+		return color.RedString(s)
+	} else if strings.HasSuffix(s, "_COMPLETE") || s == "AVAILABLE" {
+		return color.GreenString(s)
+	} else if strings.HasSuffix(s, "_IN_PROGRESS") || strings.HasSuffix(s, "_PENDING") {
+		return color.YellowString(s)
+	} else if s == cfn.StackStatusNotFound || s == "UNAVAILABLE" {
+		return color.HiBlackString(s)
+	} else if strings.HasSuffix(s, "_FAILED") {
+		return color.RedString(s)
+	}
+	return color.WhiteString(s)
 }
 
 func (o *outputCommon) Short() output {
@@ -64,159 +87,207 @@ func (o *outputCommon) Output(w io.Writer) {
 	}
 }
 
-const outputStackShortTpl = `
-{{- "Stack" | hiwhite }}:	{{ .ConfigName | cyan }}
-{{ "StackName" | hiwhite }}:	{{ .Name }}
-{{ "StackStatus" | hiwhite }}:	{{ .Status | status }} {{ .StatusReason }}
-{{- if ne .Status "STACK_NOT_FOUND" }}
-{{ "Id" | hiwhite}}:	{{ .ID }}
-{{- end }}
-`
-
-const outputStackLongTpl = outputStackShortTpl +
-	`{{ "Parameters" | hiwhite }}:
-{{- range $k, $v := .Parameters }}
-  {{ $k | hiwhite }}:	{{ $v | quote }}
-{{- end }}
-{{ "Outputs" | hiwhite }}:
-{{- range $k, $v := .Outputs }}
-  {{ $k | hiwhite }}:	{{ $v | quote }}
-{{- end }}
-`
-
-const outputStackStatusLineTpl = `{{ "info" | cyan }}: {{ .Name }} - {{ .Status | status }}{{ if .StatusReason }} - {{ .StatusReason }}{{ end }}
-`
-
-const outputChangeSetStatusLineTpl = `{{ "info" | cyan }}: change set {{ .Name }} - {{ .Status | status }}{{ if .StatusReason }} - {{ .StatusReason }}{{ end }}
-`
-
-const outputStackEventStatusLineTpl = `{{ "info" | cyan }}: {{ .StackName }}, {{ .LogicalResourceID | hiwhite }} ({{ .ResourceType }}) - ` +
-	`{{ .ResourceStatus | status }}{{ if .ResourceStatusReason }} - {{ .ResourceStatusReason }}{{ end }}
-`
-
 func outputStack(w io.Writer, stack *clon.StackData, typ int) error {
-	var tpl string
-	switch typ {
-	case outputTypeShort:
-		tpl = outputStackShortTpl
-	case outputTypeLong:
-		tpl = outputStackLongTpl
-	case outputTypeStatusLine:
-		tpl = outputStackStatusLineTpl
-	default:
-		return errors.Errorf("output type %d for stack is not implemented", typ)
+	if typ == outputTypeStatusLine {
+		fmt.Fprintf(w, "%s: stack status - %s [%s] %s\n",
+			color.CyanString("info"),
+			color.HiWhiteString(stack.Name),
+			outputStatus(stack.Status),
+			stack.StatusReason,
+		)
+		return nil
 	}
-	return errors.Trace(render(w, tpl, "", stack))
+	tw := tabwriter.NewWriter(w, 0, 0, 1, ' ', 0)
+	fmt.Fprintf(tw, "%s:\t%s\n", color.HiWhiteString("Stack"), color.CyanString(stack.ConfigName))
+	fmt.Fprintf(tw, "%s:\t%s\n", color.HiWhiteString("StackName"), stack.Name)
+	fmt.Fprintf(tw, "%s:\t%s %s\n", color.HiWhiteString("StackStatus"), outputStatus(stack.Status), stack.StatusReason)
+	if stack.Status != cfn.StackStatusNotFound {
+		fmt.Fprintf(tw, "%s:\t%s\n", color.HiWhiteString("Id"), stack.ID)
+	}
+	if typ == outputTypeLong {
+		if len(stack.Parameters) > 0 {
+			fmt.Fprintf(tw, "%s:\n", color.HiWhiteString("Parameters"))
+			for k, v := range stack.Parameters {
+				fmt.Fprintf(tw, "  %s:\t%q\n", color.HiWhiteString(k), v)
+			}
+		}
+		if len(stack.Outputs) > 0 {
+			fmt.Fprintf(tw, "%s:\n", color.HiWhiteString("Outputs"))
+			for k, v := range stack.Outputs {
+				fmt.Fprintf(tw, "  %s:\t%q\n", color.HiWhiteString(k), v)
+			}
+		}
+	}
+	return nil
 }
 
-const outputPlanLongTplDefs = `
-{{- define "changeDetails" }}
-    {{- $.Name | hiwhite }}:
-{{- range $_, $d := .Details }}
-      {{ "CausedBy" | hiwhite}}: {{ $d.ChangeSource }}
-      {{- if $d.CausingEntity }}, {{ "CausingEntity" | hiwhite}}: {{ $d.CausingEntity }}{{ end }}
-      {{- ", " }}{{ "Evaluation" | hiwhite }}: {{ $d.Evaluation }}
-    {{- if weq $d.Target.RequiresRecreation $.const.RequiresRecreationAlways }}
-      {{- red " (forces recreation)" }}
-    {{- else if weq $d.Target.RequiresRecreation $.const.RequiresRecreationConditionally }}
-      {{- hired " (conditional recreation)" }}
-    {{- end -}}
-{{ end }}
-{{- end -}}{{/* end define changeDetails */}}
+func outputPlanResourceChangeDetailsSingle(tw io.Writer, name string, details []*cloudformation.ResourceChangeDetail) {
+	if name != "" {
+		fmt.Fprintf(tw, "  %s:\n", color.HiWhiteString(name))
+	}
+	for _, d := range details {
+		fmt.Fprintf(tw, "    %s: %s, ", color.HiWhiteString("ChangeSource"), aws.StringValue(d.ChangeSource))
 
-{{- define "changes" -}}
-{{- $properties := dict -}}
-{{- $attributes := dict -}}
+		if d.CausingEntity != nil {
+			fmt.Fprintf(tw, "%s: %s, ", color.HiWhiteString("CausingEntity"), aws.StringValue(d.CausingEntity))
+		}
+		fmt.Fprintf(tw, "%s: %s", color.HiWhiteString("Evaluation"), aws.StringValue(d.Evaluation))
 
-{{- range $_, $d := .Details -}}
-  {{- if weq $d.Target.Attribute $.const.ResourceAttributeProperties -}}
-    {{- $name := $d.Target.Name | awsStringValue }}
-    {{- $_ := set $properties $name (append (default list (index $properties $name)) $d) -}}
-  {{- else -}}
-    {{- $name := $d.Target.Attribute | awsStringValue }}
-    {{- $_ := set $attributes $name (append (default list (index $attributes $name)) $d) -}}
-  {{- end -}}
-{{- end -}}
-{{- if $properties -}}
-{{- range $name, $d := $properties -}}
-{{ "    " }}{{ template "changeDetails" (merge (dict "Details" $d "Name" $name) (omit $ "Details" "Name")) }}
-{{ end }}
-{{ end }}
+		switch aws.StringValue(d.Target.RequiresRecreation) {
+		case cloudformation.RequiresRecreationAlways:
+			fmt.Fprintf(tw, " %s", color.RedString("(requires recreation)"))
+		case cloudformation.RequiresRecreationConditionally:
+			fmt.Fprintf(tw, " %s", color.HiRedString("(conditional recreation)"))
+		default:
+		}
 
-{{- if $attributes -}}
-{{- range $name, $d := $attributes -}}
-{{ "    " }}{{ template "changeDetails" (merge (dict "Details" $d "Name" $name) (omit $ "Details" "Name")) }}
-{{- end }}
-{{ end }}{{/* end if $attributes */}}
+		if configFlags.ignoreNestedUpdates && len(details) == 1 && aws.StringValue(d.ChangeSource) == "Automatic" &&
+			aws.StringValue(d.Evaluation) == "Dynamic" &&
+			aws.StringValue(d.Target.Attribute) == "Properties" &&
+			aws.StringValue(d.Target.RequiresRecreation) == "Never" {
+			fmt.Fprintf(tw, " %s", color.HiCyanString("(nested stack update check, ignored)"))
+		}
 
-{{- end -}}
-`
+		fmt.Fprintf(tw, "\n")
+	}
+}
 
-const outputPlanLongTpl = `
-{{- "Stack" | hiwhite }}:	{{ .Stack.ConfigName | cyan }}
-{{ "StackName" | hiwhite }}:	{{ .Stack.Name }}
-{{ "StackStatus" | hiwhite }}:	{{ .Stack.Status | status }} {{ .Stack.StatusReason }}
-{{- if ne .Stack.Status "STACK_NOT_FOUND" }}
-{{ "Id" | hiwhite}}:	{{ .Stack.ID }}
-{{- end }}
-{{ "ChangeSetId" | hiwhite }}:	{{ .ChangeSet.ID }}
-{{ "ChangeSetName" | hiwhite }}:	{{ .ChangeSet.Name }}
-{{ "ExecutionStatus" | hiwhite }}:	{{ .ChangeSet.ExecutionStatus | status }}
-{{ "RoleARN" | hiwhite }}:	{{ .RoleARN.String }}
+func outputPlanResourceChangeDetails(tw io.Writer, details []*cloudformation.ResourceChangeDetail) {
+	if len(details) == 0 {
+		return
+	}
+	properties := make(map[string][]*cloudformation.ResourceChangeDetail)
+	attributes := make(map[string][]*cloudformation.ResourceChangeDetail)
+	pnames := make([]string, 0)
+	anames := make([]string, 0)
+	for _, detail := range details {
+		if aws.StringValue(detail.Target.Attribute) == cloudformation.ResourceAttributeProperties {
+			name := aws.StringValue(detail.Target.Name)
+			p, ok := properties[name]
+			if !ok {
+				pnames = append(pnames, name)
+			}
+			properties[name] = append(p, detail)
+		} else {
+			name := aws.StringValue(detail.Target.Attribute)
+			a, ok := attributes[name]
+			if !ok {
+				anames = append(anames, name)
+			}
+			attributes[name] = append(a, detail)
+		}
+	}
 
-{{- if .Parameters.HasChange }}
+	if len(properties) > 0 {
+		sort.Strings(pnames)
+		for _, name := range pnames {
+			_ = name
+			outputPlanResourceChangeDetailsSingle(tw, name, properties[name])
+		}
+	}
 
-{{ "Parameters" | hiwhite }}:
-{{- range $name, $d := .Parameters }}
-  {{- if not $d.IsEqual }}
-  {{ $name | hiwhite }}:	{{ $d.String | yellow }}
-  {{- end }}
-{{- end }}
-{{- end }}
-
-{{- if .ChangeSet.Changes }}
-
-{{ "ResourceChanges" | hiwhite }}:
-{{- range $name, $r := .ChangeSet.Changes }}
-  {{- if weq $r.Action $.const.ChangeActionAdd }}
-{{ greenf "[+] "}}{{ $r.LogicalResourceId | green }} ({{ $r.ResourceType }})
-  {{- else if weq $r.Action $.const.ChangeActionRemove }}
-{{ redf "[-] "}}{{ $r.LogicalResourceId | red }} ({{ $r.ResourceType }})
-  {{- else if weq $r.Action $.const.ChangeActionModify }}
-    {{- if weq $r.Replacement $.const.ReplacementTrue }}
-{{ redf "[±] "}}{{ $r.LogicalResourceId | red }} ({{ $r.ResourceType }})
-    {{- else if weq $r.Replacement $.const.ReplacementFalse }}
-{{ yellowf "[~] "}}{{ $r.LogicalResourceId | yellow }} ({{ $r.ResourceType }})
-    {{- else }}
-{{ hiredf "[?] "}}{{ $r.LogicalResourceId | hired }} ({{ $r.ResourceType }})
-    {{- end }}
-{{ template "changes" (merge (dict "Details" $r.Details) $) }}
-  {{- end }}
-{{- end }}
-{{- end }}
-
-`
+	if len(attributes) > 0 {
+		sort.Strings(anames)
+		for _, name := range anames {
+			_ = name
+			outputPlanResourceChangeDetailsSingle(tw, name, attributes[name])
+		}
+	}
+}
 
 func outputPlan(w io.Writer, plan *clon.Plan, typ int) error {
-	if typ == outputTypeLong {
-		return errors.Trace(render(w, outputPlanLongTpl, outputPlanLongTplDefs, plan))
-	} else if typ == outputTypeShort {
+	if typ == outputTypeShort {
 		_, err := fmt.Fprintln(w, plan.ID)
 		return errors.Trace(err)
+	} else if typ != outputTypeLong {
+		return errors.Errorf("output type %d for plan is not implemented", typ)
 	}
-	return errors.Errorf("output type %d for plan is not implemented", typ)
+
+	var cw = color.HiWhiteString
+	var sv = aws.StringValue
+
+	tw := tabwriter.NewWriter(w, 0, 0, 1, ' ', 0)
+	fmt.Fprintf(tw, "%s:\t%s\n", cw("Stack"), color.CyanString(plan.Stack.ConfigName))
+	fmt.Fprintf(tw, "%s:\t%s\n", cw("StackName"), plan.Stack.Name)
+	fmt.Fprintf(tw, "%s:\t%s\n", cw("StackStatus"), outputStatus(plan.Stack.Status))
+	if plan.Stack.Status != cfn.StackStatusNotFound {
+		fmt.Fprintf(tw, "%s:\t%s\n", cw("StackID"), plan.Stack.ID)
+	}
+	fmt.Fprintf(tw, "%s:\t%s\n", cw("ChageSetID"), plan.ChangeSet.ID)
+	fmt.Fprintf(tw, "%s:\t%s\n", cw("ChangeSetName"), plan.ChangeSet.Name)
+	fmt.Fprintf(tw, "%s:\t%s\n", cw("ExecutionStatus"), outputStatus(plan.ChangeSet.ExecutionStatus))
+	fmt.Fprintf(tw, "%s:\t%s\n", cw("RoleARN"), plan.RoleARN.String())
+
+	if plan.Parameters.HasChange() {
+		fmt.Fprintf(tw, "\n%s:\n", cw("Parameters"))
+		for name, param := range plan.Parameters {
+			if param.IsEqual() {
+				continue
+			}
+			fmt.Fprintf(tw, "  %s:\t%s\n", cw(name), color.YellowString(param.String()))
+		}
+	}
+
+	if len(plan.ChangeSet.Changes) > 0 {
+		fmt.Fprintf(tw, "\n%s:\n", cw("ResourceChanges"))
+		for _, res := range plan.ChangeSet.Changes {
+			var col color.Attribute
+			var sign byte
+			switch sv(res.Action) {
+			case cloudformation.ChangeActionAdd:
+				col = color.FgGreen
+				sign = '+'
+			case cloudformation.ChangeActionRemove:
+				col = color.FgRed
+				sign = '-'
+			case cloudformation.ChangeActionModify:
+				switch sv(res.Replacement) {
+				case cloudformation.ReplacementTrue:
+					col = color.FgRed
+					sign = '±'
+				case cloudformation.ReplacementFalse:
+					col = color.FgYellow
+					sign = '~'
+				case cloudformation.ReplacementConditional:
+					col = color.FgHiRed
+					sign = '?'
+				}
+			}
+			fmt.Fprintf(tw, "%s (%s)\n", color.New(col).Sprintf("[%c] %s", sign, sv(res.LogicalResourceId)), sv(res.ResourceType))
+			outputPlanResourceChangeDetails(tw, res.Details)
+			fmt.Fprintf(tw, "\n")
+		}
+	}
+
+	tw.Flush()
+
+	return nil
 }
 
 func outputChangeSet(w io.Writer, cs *cfn.ChangeSetData, typ int) error {
 	if typ != outputTypeStatusLine {
 		return errors.Errorf("output type %d for change set is not implemented", typ)
 	}
-	return errors.Trace(render(w, outputChangeSetStatusLineTpl, "", cs))
+	fmt.Fprintf(w, "%s: changeset status - %s [%s] %s\n",
+		color.CyanString("info"),
+		color.HiWhiteString(cs.Name),
+		outputStatus(cs.Status),
+		cs.StatusReason,
+	)
+	return nil
 }
 
 func outputStackEvent(w io.Writer, cs *cfn.StackEventData, typ int) error {
 	if typ != outputTypeStatusLine {
 		return errors.Errorf("output type %d for change set is not implemented", typ)
 	}
-	return errors.Trace(render(w, outputStackEventStatusLineTpl, "", cs))
+	fmt.Fprintf(w, "%s: resource status - %s:%s (%s) - [%s] %s\n",
+		color.CyanString("info"),
+		color.HiWhiteString(cs.StackName),
+		color.HiWhiteString(cs.LogicalResourceID),
+		cs.ResourceType,
+		outputStatus(cs.ResourceStatus),
+		cs.ResourceStatusReason,
+	)
+	return nil
 }
